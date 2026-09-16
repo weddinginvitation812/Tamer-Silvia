917a235a204b46d7713e51b8843c4dc6a2397de6
weddinginvitation812 <weddinginvitation405@gmail.com>
2026-09-16 23:18:28 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const WEDDING = {
+  groomName: "Tamer",
+  brideName: "Silvia",
+  initials: "T S",
+  weddingDateTime: "2026-10-11T21:00:00",
+  dateDisplay: "11.10.2026",
+  dayDisplay: "MONDAY",
+  timeDisplay: "AT 9:00 PM",
+  church: "كنيسة القديس العظيم مارجرجس , ابنوب",
+  venue: "Royal plaza Hall ",
+  
+  musicFile: "music/song.mp3",
+  musicTitle: "Our Love Song",
+  designedBy: "DESIGNED BY Youssef Ehab"
+};
